1c3111254896d60949540cf7e81b7d55b35623f3
Macnimo <david.macnimo@gmail.com>
2023-01-10 10:19:31 +0100

    Example of Model : AnnoncesModel.php

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
-use App\Models\AnnoncesModel;
 // header("Access-Control-Allow-Origin: http://localhost:5173");
 // header("Content-Type: text/html; charset=utf-8");
 
 use App\Autoloader;
+use App\Models\AnnoncesModel;
+
 require_once 'Autoloader.php';
 Autoloader::register();
 
-$model = new AnnoncesModel();
+$model = new AnnoncesModel;
 
-var_dump($model->findAll());
+$annonce = $model->setTitre('Nouvelle annonce');
+
+var_dump($annonce);
 // phpinfo();
