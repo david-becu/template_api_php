cb0dbc3b4865ae945ce31ddde206c2fcaafc97e6
Macnimo <david.macnimo@gmail.com>
2023-01-10 08:31:45 +0100

    add findBy method

--- Models/Model.php
+++ Models/Model.php
@@ -18,6 +18,26 @@ class Model extends Db
         return $query->fetchAll();
     }
 
+    public function findBy(array $criteres)
+    {
+        $champs = [];
+        $valeurs = [];
+
+        // On boucle pour éclater le tableau
+        foreach ($criteres as $champ => $valeur) {
+            // SELECT * FROM table WHERE actif = ? AND signale = 0
+            // bindValue(1, valeur)
+            $champs[] = "$champ = ?";
+            $valeurs[] = $valeur;
+        }
+
+        // On transforme le tableau "champs" en une chaine de caractères
+        $liste_champs = implode(' AND ', $champs);
+
+        // On exécute la requête
+        return $this->requete("SELECT * FROM {$this->table} WHERE $liste_champs", $valeurs)->fetchAll();
+    }
+
     public function requete(string $sql, array $attributs = null)
     {
         // On récupére l'instance de Db
